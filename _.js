79c567d1c1f67a059956bd4025e6f97ed047568f
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-03-24 11:44:07 +0100

    Deleted the console.log tests in _.js

--- _.js
+++ _.js
@@ -6,10 +6,5 @@ let _ = {
     }
 };
 
-console.log(_.clamp(0,1,3));
-console.log(_.clamp(1,2,4));
-console.log(_.clamp(5,2,4));
-
-
 // Do not write or modify code below this line.
 module.exports = _;
